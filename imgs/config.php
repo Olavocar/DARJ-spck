3578436d1c717bf2d68404f697814d5b3c05be4d
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-19 08:28:18 -0300

    Update config.php

--- imgs/config.php
+++ imgs/config.php
@@ -1,10 +1,10 @@
 <?php
-    
+    /*
     $dbHost = 'https://www.db4free.net';
     $dbUsername = 'volaodarj';
     $dbPassword = '71217ca8';
     $dbName = 'daarearj';
-    
+    */
     $conexao = new mysqli('localhost:3306','volaodarj','71217ca8','daarearj');
 
     if($conexao->connect_errno)
