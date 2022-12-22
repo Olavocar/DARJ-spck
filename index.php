71e31c37f5e0eda934283bdeffde270b903283e6
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-22 11:31:05 -0300

    commit heroku 2

--- index.php
+++ index.php
@@ -4,82 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width-device-width, initial-scale-1">
 
-        <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
 
         <title>Cadastro</title>
 
     </head>
     <body>
-        
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Cadastro de Usuários</a>
-    
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-    
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-        
-                        <li class="nav-item">
-                            <a class="nav-link" href="?page=novo">Novo Usuário</a>
-                        </li>
-        
-                        <li class="nav-item">
-                            <a class="nav-link" href="?page=listar">Listar Usuários</a>
-                        
-                        
-                        
-                        
-                        
-                        
-                
-                            </li>
-     
-        
-      </ul>
-
-    </div>
-  </div>
-</nav>
-
 
 <div class="container">
     <div class="row">
         <div class="col mt-5">
 <?php
 
-            include("config.php");
-            switch($_REQUEST["page"]){
-                case "novo":
-                    include("novo-usuario.php");
-            break;
-                case "listar":
-                    include("listar-usuarios.php");
-            break;
-                case "salvar":
-                    include("salvar-usuario.php");
-            break;
-                case "editar":
-                    include("editar-usuario.php");
-            break;
-                default:
-                    print "<h1>Bem Vindos</h1>";
-                }
-
+            include('config.php');
+            
 ?>
         </div>
     </div>
 </div>
 
-<form  action="index.html" method ="POST">
+<form  action="" method ="POST">
 <div class="box">
         <fieldset class="field">Da Área RJ              
             <br> <br> <label class=" cadastrar">Cadastre-se:</label>
