c5c5ff94af29073605fd67bf1ece7815b9cbdd0f
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-23 11:45:46 -0300

    commit 22

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(empty($_POST['email']) || empty($_POST['senha'])) {
 $usuario = mysqli_real_escape_string($conn, $_POST['email']);
 $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 
-$query = "select email from cadastro where email = '{$email}' and senha = md5('{$senha}')";
+$query = "select email from cadastro where email = '{$email}' and senha = '{$senha}'";
 
 $result = mysqli_query($conn, $query);
 
