91de6161d4622c63a75e1c5aec0e4906adbc0c9b
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-23 09:36:57 -0300

    commit heroku

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+              $dbHost = 'db4free.net:3306';
+              $dbUsername = 'volaodarj';
+              $dbPassword = '26ec018b';
+              $dbname = 'daarearj';
+          
+              $conn = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbname);
+
+            if(isset($_POST['submit']))
+    {
+            $nome = $_POST['nome'];
+            $sobrenome = $_POST['sobrenome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $telefone = $_POST['telefone'];
+            $data_nasc = $_POST['data_nasc'];        
+            $query = mysqli_query($conn, "INSERT INTO cadastro (nome, sobrenome, email, senha, telefone, data_nasc) VALUES ('$nome', '$sobrenome', '$email', '$senha', '$telefone', '$data_nasc')");
+    }
+        if($query){
+        echo 'Cadastro realizado com sucesso';
+        header('Location: indexlogin.php');
+        exit();
+        }   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-device-width, initial-scale-1">
+
+        <link href="style.css" rel="stylesheet">
+
+        <title>Cadastro</title>
+</head>
+ <body>
+  <div class="container">
+    <div class="row">
+     <div class="col mt-5">
+ <form action="index.php" method ="POST">
+ <div class="box">
+        <fieldset class="field">Da Área RJ              
+            <br> <br> <label class="cadastrar">Cadastre-se:</label>
+                    <br><br>
+                        <label for="nome" class="inputtext">Nome:</label>
+                            <br>
+                            <input type="text"  name="nome" id="nome" class="inputUser" required/>  
+                            <br>
+                            <label for="sobrenome" class="inputtext">Sobrenome:</label>
+                            <br>               
+                            <input type="text"  name="sobrenome" id="sobrenome" class="inputUser" required/>
+                            <br>
+                            <br>
+                            <label for="email" class="inputtext">Email:</label>
+                            <br>
+                            <input type="email" name="email" class="form-control" required>
+                            <br>
+                            <label>Cadastre Sua Senha:</label><br>
+                            <input type="password" name="senha" id="senha" class="form-control" required>
+                            <br><br>
+                            <label for="telefone" class="inputtext">Telefone:</label>
+                            <br>
+                            <input type="text"  name="telefone" id="telefone" class="inputUser" required/>
+                            <br>
+                            <br>
+                            <label for="data_nasc" class="inputtext">Data de Nascimento:</label>
+                            <br>    
+                            <input type="date"  name="data_nasc" id="data_nasc" class="form-control" required/>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <button class="btn btn-primary" name="submit" id="submit" type="submit">Enviar</button>
+                            <fiedlset>
+                            Já é um usuário registrado? <br>
+                            <a href="indexlogin.php">Acesse aqui</a>
+                            </fieldset>
+        </fieldset>
+      </div>
+     </form>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
+
+<!--
+<li class="nav-item dropdown">
+     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+ -->
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include('conn.php');
+
+if(empty($_POST['email']) || empty($_POST['senha'])) {
+	header('Location: index.php');
+	exit();
+}
+
+$usuario = mysqli_real_escape_string($conn, $_POST['email']);
+$senha = mysqli_real_escape_string($conn, $_POST['senha']);
+
+$query = "select usuario from usuario where usuario = '{$email}' and senha = md5('{$senha}')";
+
+$result = mysqli_query($conn, $query);
+
+$row = mysqli_num_rows($result);
+
+if($row == 1) {
+	$_SESSION['email'] = $email;
+	header('Location: painel.php');
+	exit();
+} else {
+	$_SESSION['nao_autenticado'] = true;
+	header('Location: index.php');
+	exit();
+}
